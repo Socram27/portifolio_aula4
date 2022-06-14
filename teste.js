e3d86fefae7271779f4724f11ee6d63f18f833ff
Marcos Roberto <m.r.oliveira0912@gmail.com>
2022-06-13 22:14:49 -0300

    Conteúdo de JavaScript

---
+++ teste.js
@@ -0,0 +1,7 @@
+alert('Olá! Seja Bem-vindo!');
+document.querySelector('#t1').textContent = 'JavaScript - Introdução à Linguagem';
+document.write('Essa é nossa primeira vídeo aula.');
+
+const nome = prompt('Qual é o seu nome');
+
+alert(`Que bom ter você aqui ${nome} \n Vamos começar.`);
